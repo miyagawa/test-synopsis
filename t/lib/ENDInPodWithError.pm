b178a674d9c75f7e9e22d1933d2cc2803759b0d2
Zoffix Znet <cpan@zoffix.com>
2014-02-04 19:14:03 -0500

    Added t/02-END-token-errors.t

---
+++ t/lib/ENDInPodWithError.pm
@@ -0,0 +1,32 @@
+package Test::Synopsis::__TestBait_ENDinPodWithError;
+
+# Dummy module used during testing of Test::Synopsis. Needs to be in the lib/
+# dir of the Test-Synopsis distribution to Test::Synopsis can find it.
+#
+# This module does not has an __END__ in the synopsis code; it shouldn't
+# break Test::Synopsis
+
+use strict;
+use warnings;
+
+# VERSION
+
+1;
+
+=pod
+
+=head1 SYNOPSIS
+
+Testing stuff:
+
+    print "Foos!\n"; ****** ZOMG! A ERRORZ! :D
+
+    __END__
+
+    BLARGHS!
+
+=head1 DESCRIPTION
+
+bleh
+
+=cut
